906a1fdab557b47216b0f389d96734aaf38c439e
Andres Hortua <fandresh7@gmail.com>
2024-11-03 18:31:17 -0500

    remove animation on addTodo

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,10 +1,10 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core'
 import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop'
 import { TodoComponent } from '../todo/todo.component'
+
 import { TodoListFooterComponent } from '../todo-list-footer/todo-list-footer.component'
 import { TodosStore } from '../../store/todos.store'
 import { Todo } from '../../models/todo'
-import { trigger, transition, style, animate } from '@angular/animations'
 
 @Component({
   selector: 'todo-list',
@@ -12,8 +12,7 @@ import { trigger, transition, style, animate } from '@angular/animations'
   imports: [DragDropModule, TodoComponent, TodoListFooterComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  animations: [trigger('todoAnimation', [transition(':enter', [style({ opacity: 0, transform: 'translateY(-20px)' }), animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))])])]
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoListComponent {
   store = inject(TodosStore)
