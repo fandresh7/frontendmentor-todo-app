1343c68bbc524c19c7db2783cc46ed8e8388b450
Andres Hortua <fandresh7@gmail.com>
2024-11-03 18:36:55 -0500

    add loading to changestatus and delete todo

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,6 +1,6 @@
-import { ChangeDetectionStrategy, Component, inject, input } from '@angular/core'
+import { ChangeDetectionStrategy, Component, inject, input, signal } from '@angular/core'
 import { CheckInputComponent } from '../check-input/check-input.component'
-import { IconCrossComponent } from '../../shared/components/icons/icons.component'
+import { IconCrossComponent, IconSpinComponent } from '../../shared/components/icons/icons.component'
 import { Todo } from '../../models/todo'
 import { NgClass } from '@angular/common'
 import { TodosStore } from '../../store/todos.store'
@@ -8,7 +8,7 @@ import { TodosStore } from '../../store/todos.store'
 @Component({
   selector: 'todo',
   standalone: true,
-  imports: [CheckInputComponent, IconCrossComponent, NgClass],
+  imports: [CheckInputComponent, IconCrossComponent, NgClass, IconSpinComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -18,13 +18,23 @@ export class TodoComponent {
 
   todo = input.required<Todo>()
 
+  loading = signal<boolean>(false)
+
   async changeIsChecked(value: boolean) {
+    this.loading.set(true)
+
     const status = value ? 'completed' : 'active'
     await this.store.changeTodoStatus(this.todo().id, status)
+
+    this.loading.set(false)
   }
 
   async deleteTodo() {
+    this.loading.set(true)
+
     const id = this.todo().id
     await this.store.deleteTodo(id)
+
+    this.loading.set(false)
   }
 }
