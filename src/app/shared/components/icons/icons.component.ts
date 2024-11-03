7dfed1e5184440bea2bb4e35368bc9080403a078
Andres Hortua <fandresh7@gmail.com>
2024-11-03 18:49:04 -0500

    improve loading on todo

--- src/app/shared/components/icons/icons.component.ts
+++ src/app/shared/components/icons/icons.component.ts
@@ -69,7 +69,7 @@ export class IconSunComponent {}
   selector: 'icon-spin',
   standalone: true,
   template: `<svg
-    class="animate-spin -ml-1 mr-3 h-5 w-5"
+    class="animate-spin h-5 w-5"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24">
