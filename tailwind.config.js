799e620df5b6e07efec72fd3058a36a1253d584d
Andres Hortua <fandresh7@gmail.com>
2024-11-02 20:05:24 -0500

    add font and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'sans': ['Josefin Sans', 'ui-sans-serif', 'system-ui'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
