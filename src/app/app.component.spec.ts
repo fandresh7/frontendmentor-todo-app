575c860912eedaa190b716035bb64cf629583e99
Andres Hortua <fandresh7@gmail.com>
2024-11-04 15:26:20 -0500

    add testing to app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,36 @@
-import { TestBed } from '@angular/core/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 import { AppComponent } from './app.component'
 
-describe('AppComponent', () => {
+fdescribe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>
+  let compiled: HTMLElement
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent]
     }).compileComponents()
+
+    fixture = TestBed.createComponent(AppComponent)
+    compiled = fixture.nativeElement as HTMLElement
   })
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.componentInstance
     expect(app).toBeTruthy()
+  })
+
+  it('should have main-header component', () => {
+    const mainHeader = compiled.querySelector('main-header')
+    expect(mainHeader).not.toBeNull()
+  })
+
+  it('should have todo-list component', () => {
+    const todoList = compiled.querySelector('todo-list')
+    expect(todoList).not.toBeNull()
+  })
+
+  it('should have filters component', () => {
+    const filters = compiled.querySelector('filters')
+    expect(filters).not.toBeNull()
   })
 })
