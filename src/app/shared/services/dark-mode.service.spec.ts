5a01567e6131647ef5ecffd0ce04cd9e7b166664
Andres Hortua <fandresh7@gmail.com>
2024-11-04 21:10:59 -0500

    add testing for localstorage and darkmode services

--- src/app/shared/services/dark-mode.service.spec.ts
+++ src/app/shared/services/dark-mode.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing'
-
 import { DarkModeService } from './dark-mode.service'
+import { LocalStorageService } from './local-storage.service'
+
+class MockLocalStorageService {
+  setItem = jasmine.createSpy('setItem')
+  getItem = jasmine.createSpy('getItem')
+}
 
 describe('DarkModeService', () => {
   let service: DarkModeService
+  let mockLocalStorageService: MockLocalStorageService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: LocalStorageService,
+          useClass: MockLocalStorageService
+        }
+      ]
+    })
+
     service = TestBed.inject(DarkModeService)
+    mockLocalStorageService = TestBed.inject(LocalStorageService) as unknown as MockLocalStorageService
   })
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should toggle dark Mode to true, then to false when toggleDarkMode is called', () => {
+    service.toggleDarkMode()
+    expect(service.darkMode()).toBe(true)
+
+    service.toggleDarkMode()
+    expect(service.darkMode()).toBe(false)
+  })
+
+  it('should set dark mode to true if "theme" in localStorage is "dark"', () => {
+    mockLocalStorageService.getItem.and.returnValue('dark')
+    expect(service.darkMode()).toBe(true)
   })
 })
--- src/app/shared/services/local-storage.service.spec.ts
+++ src/app/shared/services/local-storage.service.spec.ts
@@ -8,9 +8,39 @@ describe('LocalStorageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({})
     service = TestBed.inject(LocalStorageService)
+
+    spyOn(localStorage, 'getItem').and.callFake((key: string) => {
+      return key === 'testKey' ? 'testValue' : null
+    })
+
+    spyOn(localStorage, 'setItem')
+    spyOn(localStorage, 'removeItem')
+    spyOn(localStorage, 'clear')
   })
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('should return an item from localstorage', () => {
+    const ls = service.getItem('testKey')
+
+    expect(ls).toBe('testValue')
+    expect(localStorage.getItem).toHaveBeenCalledWith('testKey')
+  })
+
+  it('should set an item in localstorage', () => {
+    service.setItem('testKey', 'testValue')
+    expect(localStorage.setItem).toHaveBeenCalledWith('testKey', 'testValue')
+  })
+
+  it('should remove item from localstorage', () => {
+    service.removeItem('testKey')
+    expect(localStorage.removeItem).toHaveBeenCalledWith('testKey')
+  })
+
+  it('should clear localstorage', () => {
+    service.clear()
+    expect(localStorage.clear).toHaveBeenCalled()
   })
 })
