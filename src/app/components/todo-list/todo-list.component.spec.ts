c451cd7c69d443f4a5642d31d1eeb579a4878565
Andres Hortua <fandresh7@gmail.com>
2024-11-05 20:01:18 -0500

    add testing for todo and todo list components

--- src/app/components/todo-list/todo-list.component.spec.ts
+++ src/app/components/todo-list/todo-list.component.spec.ts
@@ -1,23 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { CdkDragDrop, CdkDropList } from '@angular/cdk/drag-drop'
 
 import { TodoListComponent } from './todo-list.component'
+import { TodosStore } from '../../store/todos.store'
+import { TODOS_MOCK } from '../../models/mock-data'
+import { Todo } from '../../models/todo'
+
+class MockTodosStore {
+  reorder = jasmine.createSpy('reorder').and.resolveTo()
+  filteredTodos = jasmine.createSpy('filteredTodos').and.returnValue(TODOS_MOCK)
+  activeTodosAmount = jasmine.createSpy('activeTodosAmount')
+  filter = jasmine.createSpy('filter')
+}
 
 describe('TodoListComponent', () => {
   let component: TodoListComponent
-  // let compiled: HTMLElement
+  let compiled: HTMLElement
   let fixture: ComponentFixture<TodoListComponent>
+
+  let store: MockTodosStore
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TodoListComponent]
+      imports: [TodoListComponent],
+      providers: [
+        {
+          provide: TodosStore,
+          useClass: MockTodosStore
+        }
+      ]
     }).compileComponents()
 
     fixture = TestBed.createComponent(TodoListComponent)
-    // compiled = fixture.nativeElement as HTMLElement
+    compiled = fixture.nativeElement as HTMLElement
     component = fixture.componentInstance
+
+    store = TestBed.inject(TodosStore) as unknown as MockTodosStore
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should call reorder method with the correct todo id and new index on drop event', async () => {
+    const previousIndex = 0
+    const currentIndex = 2
+    const todo = store.filteredTodos()[previousIndex]
+
+    const mockEvent: Partial<CdkDragDrop<Todo[]>> = {
+      previousIndex,
+      currentIndex,
+      container: {
+        data: store.filteredTodos()
+      } as CdkDropList<Todo[]>
+    }
+
+    await component.drop(mockEvent as CdkDragDrop<Todo[]>)
+    expect(store.reorder).toHaveBeenCalledWith(todo.id, currentIndex)
+  })
+
+  it('should render the list of todos when they are available', () => {
+    fixture.detectChanges()
+
+    const todoElements = compiled.querySelectorAll('todo')
+    const articleElement = compiled.querySelector('#empty_message')
+
+    expect(todoElements.length).toBe(store.filteredTodos().length)
+    expect(articleElement).toBeNull()
+
+    todoElements.forEach(todo => {
+      expect(todo.classList.contains('cdk-drag')).toBe(true)
+    })
+  })
+
+  it('should display an empty message when there are no todos', () => {
+    store.filteredTodos = jasmine.createSpy('filteredTodos').and.returnValue([])
+
+    fixture.detectChanges()
+
+    const todoElements = compiled.querySelectorAll('todo')
+    const articleElement = compiled.querySelector('#empty_message')
+
+    expect(todoElements.length).toBe(0)
+    expect(articleElement).toBeDefined()
+  })
+
+  it('should render the TodoListFooter component', () => {
+    const footerElement = compiled.querySelector('todo-list-footer')
+    expect(footerElement).toBeDefined()
   })
 })
--- src/app/components/todo/todo.component.spec.ts
+++ src/app/components/todo/todo.component.spec.ts
@@ -1,22 +1,129 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { TodoComponent } from './todo.component'
+import { TodosStore } from '../../store/todos.store'
+import { TODOS_MOCK } from '../../models/mock-data'
+
+class MockTodosStore {
+  changeTodoStatus = jasmine.createSpy('changeTodoStatus').and.resolveTo()
+  deleteTodo = jasmine.createSpy('deleteTodo').and.resolveTo()
+  filteredTodos = jasmine.createSpy('filteredTodos')
+  activeTodosAmount = jasmine.createSpy('activeTodosAmount')
+  filter = jasmine.createSpy('filter')
+}
 
 describe('TodoComponent', () => {
   let component: TodoComponent
+  let compiled: HTMLElement
   let fixture: ComponentFixture<TodoComponent>
+
+  let store: MockTodosStore
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TodoComponent]
+      imports: [TodoComponent],
+      providers: [
+        {
+          provide: TodosStore,
+          useClass: MockTodosStore
+        }
+      ]
     }).compileComponents()
 
     fixture = TestBed.createComponent(TodoComponent)
+    compiled = fixture.nativeElement as HTMLElement
     component = fixture.componentInstance
-    fixture.detectChanges()
+
+    fixture.componentRef.setInput('todo', TODOS_MOCK[0])
+
+    store = TestBed.inject(TodosStore) as unknown as MockTodosStore
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should triger changeTodoStatus when changeIsChecked is triggered', async () => {
+    fixture.detectChanges()
+
+    await component.changeIsChecked(true)
+    const todo = component.todo()
+
+    expect(store.changeTodoStatus).toHaveBeenCalledWith(todo.id, 'completed')
+  })
+
+  it('should call changeTodoStatus with the correct parameters when changeIsChecked is triggered', async () => {
+    fixture.detectChanges()
+
+    await component.changeIsChecked(true)
+    const todo = component.todo()
+
+    expect(store.changeTodoStatus).toHaveBeenCalledWith(todo.id, 'completed')
+  })
+
+  it('should set loading to true before calling changeTodoStatus, and set it to false after completion', async () => {
+    spyOn(component.loading, 'set')
+    fixture.detectChanges()
+
+    await component.changeIsChecked(true)
+
+    expect(component.loading.set).toHaveBeenCalledWith(true)
+    expect(component.loading.set).toHaveBeenCalledWith(false)
+  })
+
+  it('should call deleteTodo in the store with the correct id', async () => {
+    fixture.detectChanges()
+
+    const todoId = component.todo().id
+
+    await component.deleteTodo()
+
+    expect(store.deleteTodo).toHaveBeenCalledWith(todoId)
+  })
+
+  it('should set loading to true before calling deleteTodo, and set it to false after completion', async () => {
+    spyOn(component.loading, 'set')
+    fixture.detectChanges()
+
+    await component.deleteTodo()
+
+    expect(component.loading.set).toHaveBeenCalledWith(true)
+    expect(component.loading.set).toHaveBeenCalledWith(false)
+  })
+
+  it('should render checkInput component', () => {
+    const checkInputElement = compiled.querySelector('check-input')
+    expect(checkInputElement).not.toBeNull()
+  })
+
+  it('should render todo title', () => {
+    const todo = component.todo()
+
+    fixture.detectChanges()
+
+    const spanElement = compiled.querySelector('span')
+    expect(spanElement?.innerText).toBe(todo.title)
+  })
+
+  it('should title element have isCompleted classes if todo is completed', () => {
+    fixture.detectChanges()
+
+    const spanElement = compiled.querySelector('span')
+    expect(spanElement?.classList.contains('text-[#D1D2DA]')).toBe(true)
+    expect(spanElement?.classList.contains('line-through')).toBe(true)
+    expect(spanElement?.classList.contains('dark:text-[#4D5067]')).toBe(true)
+  })
+
+  it('should title element not have isCompleted classes if todo is active', () => {
+    const todo = TODOS_MOCK[0]
+    todo.status = 'active'
+
+    fixture.componentRef.setInput('todo', todo)
+    fixture.detectChanges()
+
+    const spanElement = compiled.querySelector('span')
+    expect(spanElement?.classList.contains('text-[#D1D2DA]')).toBe(false)
+    expect(spanElement?.classList.contains('line-through')).toBe(false)
+    expect(spanElement?.classList.contains('dark:text-[#4D5067]')).toBe(false)
   })
 })
